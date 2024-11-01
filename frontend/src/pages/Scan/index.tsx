262890abb1a6ab2230b5840dea08f14ce845f8cf
stephanieolaiya <stephanie_olaiya@brown.edu>
2024-10-31 21:39:36 -0400

    Move PagesToFileCollection to utils as a function

--- frontend/src/pages/Scan/index.tsx
+++ frontend/src/pages/Scan/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { AccessibilityResults, FileCollection, FileData, FileType, Page } from '../../interfaces/scanInterfaces';
+import { AccessibilityResults, FileCollection, Page } from '../../interfaces/scanInterfaces';
 import {
   ResizableHandle,
   ResizablePanel,
@@ -8,64 +8,17 @@ import {
 import { View } from "./View"
 import { AccessiblityPanel } from './AccessiblityPanel';
 import { useLocation } from 'react-router-dom';
-import { insertAxeScriptHTML } from './utils';
+import { PagesToFileCollection } from './utils';
 
 export function Scan() {
   // get pages from home page
   const location = useLocation();
   const { pages } = location.state as { pages: Page[] };
-  
+
   // Convert pages: Page[] to fileCollectionData that we are taking 
   // as input to AccessibilityPanel and extract framework
-  const initialFileCollection: FileCollection = {}
-  pages.map((page) => {
-    let pageCode = ""
-    // update .html files to run axe-scripts
-    if (page.pageContent.fileType === FileType.Html) {
-      pageCode = insertAxeScriptHTML(page.pageContent.body.originalVersion)
-    } else {
-      pageCode = page.pageContent.body.originalVersion
-    }
-    const pageInfo: FileData = {
-      type: page.pageContent.fileType,
-      content: pageCode
-    }
-    return initialFileCollection["/" + page.filePath.substring(page.filePath.indexOf('/')+1)] = pageInfo
-  });
-  //Add axe-script here and file validation
-  initialFileCollection["/axe-script.js"] = {
-    type: FileType.Js,
-    content: `
-            import axe from 'axe-core';
-              
-            axe.run().then((results) =>
-              window.parent.postMessage({ type: 'axeResults', results }, '*')
-              );
-          `
-  }
-  initialFileCollection["/package.json"] = {
-  type: FileType.Json, 
-  content: `{
-  "name": "html-css-js",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.html",
-  "scripts": {
-    "start": "parcel index.html --open",
-    "build": "parcel build index.html"
-  },
-  "dependencies": {"axe-core": "^4.10.1"},
-  "devDependencies": {
-    "@babel/core": "7.2.0",
-    "parcel-bundler": "^1.6.1",
-    "axe-core": "^4.10.1"
-  },
-  "keywords": []
-}`
-  }
-
-
-
+  const initialFileCollection: FileCollection = PagesToFileCollection(pages)
+  
   const emptyResults = {
     passes: [],
     violations: [],
--- frontend/src/pages/Scan/utils.ts
+++ frontend/src/pages/Scan/utils.ts
@@ -1,4 +1,4 @@
-import { FileCollection } from "@/src/interfaces/scanInterfaces"
+import { FileCollection, FileData, FileType, Framework, Page } from "../../interfaces/scanInterfaces"
 import { SandpackFiles } from "@codesandbox/sandpack-react/types"
 
 export function fileCollectionToSandPackFiles(fileCollectionData: FileCollection): SandpackFiles {
@@ -11,14 +11,74 @@ export function fileCollectionToSandPackFiles(fileCollectionData: FileCollection
     return organizedFiles
 }
 
-export function insertAxeScriptHTML(htmlContent: string){
+export function insertAxeScriptHTML(htmlContent: string) {
     const bodyEndIndex = htmlContent.indexOf("</body>")
-    const newHTML = htmlContent.substring(0, bodyEndIndex) + 
-    `
+    const newHTML = htmlContent.substring(0, bodyEndIndex) +
+        `
     <script src="node_modules/axe-core/axe.min.js"></script>
     <script src="axe-script.js"></script>
     `
-     + htmlContent.substring(bodyEndIndex);
+        + htmlContent.substring(bodyEndIndex);
     return newHTML
 }
 
+export function PagesToFileCollection(pages: Page[]) {
+    const initialFileCollection: FileCollection = {}
+    switch (pages[0].pageContent.framework) {
+        case Framework.VanillaProject:
+            pages.map((page) => {
+                let pageCode = ""
+                // update .html files to run axe-scripts
+                if (page.pageContent.fileType === FileType.Html) {
+                    pageCode = insertAxeScriptHTML(page.pageContent.body.originalVersion)
+                } else {
+                    pageCode = page.pageContent.body.originalVersion
+                }
+                const pageInfo: FileData = {
+                    type: page.pageContent.fileType,
+                    content: pageCode
+                }
+                return initialFileCollection["/" + page.filePath.substring(page.filePath.indexOf('/') + 1)] = pageInfo
+            });
+            //Add axe-script here and file validation
+            initialFileCollection["/axe-script.js"] = {
+                type: FileType.Js,
+                content: `
+                        import axe from 'axe-core';
+                          
+                        axe.run().then((results) =>
+                          window.parent.postMessage({ type: 'axeResults', results }, '*')
+                          );
+                      `
+            }
+            initialFileCollection["/package.json"] = {
+                type: FileType.Json,
+                content: `{
+              "name": "html-css-js",
+              "version": "1.0.0",
+              "description": "",
+              "main": "index.html",
+              "scripts": {
+                "start": "parcel index.html --open",
+                "build": "parcel build index.html"
+              },
+              "dependencies": {"axe-core": "^4.10.1"},
+              "devDependencies": {
+                "@babel/core": "7.2.0",
+                "parcel-bundler": "^1.6.1",
+                "axe-core": "^4.10.1"
+              },
+              "keywords": []
+            }`
+            }
+            return initialFileCollection
+        case Framework.Angular:
+            return {} as FileCollection
+        case Framework.React:
+            return {} as FileCollection
+        case Framework.Vue:
+            return {} as FileCollection
+        default:
+            return {} as FileCollection
+    }
+}
